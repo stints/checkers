8cb0cde9b8bebbcfe4a726c61e4599a971e75727
John Stinson <john.towery.stinson@gmail.com>
2016-05-02 00:31:40 -0500

    updated game logic

--- app/game.js
+++ app/game.js
@@ -1,196 +1,336 @@
-var board
+class Player {
+  constructor(name, color) {
+    this._name = name;
+    this._color = color;
+    this._pieces = [];
+  }
+
+  addPiece(piece) {
+    this._pieces.push(piece);
+  }
+
+  get pieces() {
+    return this._pieces
+  }
+
+  get pieceColor() {
+    return this._color;
+  }
+}
 
 class Game {
   constructor(id) {
-    this.canvas = document.getElementById(id)
-    this.ctx = this.canvas.getContext( "2d" )
-    board = new Board(this.ctx, this.canvas)
-    this.listeners = new Listeners(this)
+    this._canvas = document.getElementById(id);
+    this._ctx = this._canvas.getContext("2d");
+    this._players = new Array(2);
+    this._pieces = [];
+    this._board = new Array(8);
+
+
+    this._currentPlayer = null
+    this._movePiece = null // The piece to move
+    this._availableMoves = [] // Tiles the movePiece can jump to
+    this._moveSpot = null // The tile to move to
   }
 
   setup() {
-    board.setup()
-    this.listeners.setup()
-  }
+    // Create players
+    this._players[0] = new Player("player one", "red");
+    this._players[1] = new Player("player two", "white");
+    this._currentPlayer = this._players[0];
 
-  static update() {
-    board.draw()
-
-    window.requestAnimationFrame(Game.update)
-  }
-}
-
-class Listeners {
-  constructor(game) {
-    this.game = game
-    this.canvas = game.canvas
-  }
-
-  setup() {
-    this.canvas.addEventListener("mousemove", this.mouseMove)
-    this.canvas.addEventListener("click", this.mouseClick)
-  }
-
-  mouseMove(e) {
-    var rect = this.getBoundingClientRect()
-    var info = document.getElementById("hover-info")
-    var x = (e.clientX - rect.left)
-    var y = (e.clientY - rect.top)
-    var msg = x + ", " + y
-    var piece = undefined
-    for(var row = 0; row < board.rows; row++) {
-      for(var col = 0; col < board.columns; col++) {
-        var tile = board.tiles[row][col]
-        tile.highlight = tile.entered(x,y)
-        if(tile.piece !== undefined && tile.piece.clicked) {
-          piece = tile.piece
-        }
+    // Create pieces and add to player
+    for(var i = 0; i < this._players.length; i++) {
+      var player = this._players[i];
+      for(var j = 0; j < 12; j++) {
+        var piece = new Piece(player, (i+1)-(i*3));
+        this._pieces.push(piece);
+        player.addPiece(piece);
       }
     }
-    if(piece !== undefined) {
-      piece.x = x
-      piece.y = y
-    }
-    info.innerHTML = msg
-  }
 
-  mouseClick(e) {
-    var rect = this.getBoundingClientRect()
-    var x = (e.clientX - rect.left)
-    var y = (e.clientY - rect.top)
-    for(var row = 0; row < board.rows; row++) {
-      for(var col = 0; col < board.columns; col++) {
-        var tile = board.tiles[row][col]
-        tile.clicked = tile.clicked ? false : tile.entered(x,y)
-        if(tile.piece !== undefined) {
-          tile.piece.clicked = tile.clicked
-        }
+    // Create board tiles
+    for(var row = 0; row < 8; row++) {
+      this._board[row] = new Array(8);
+      for(var col = 0; col < 8; col++) {
+        var color = (row + col) % 2 ? "yellow" : "green"
+        var tile = new Tile(row, col, color)
+        this._board[row][col] = tile
       }
     }
-  }
-}
 
-class Board {
-  constructor(ctx, canvas, rows = 8, columns = 8, tile_size = 75, color1 = "black", color2 = "white") {
-    this.ctx = ctx
-    this.canvas = canvas
-    this.rows = rows
-    this.columns = columns
-    this.tile_size = tile_size
-    this.tiles = new Array(rows)
-    for( var row = 0; row < rows; row++ ) {
-      this.tiles[row] = new Array(columns)
-      for( var col = 0; col < columns; col++ ) {
-        var x = col * tile_size
-        var y = row * tile_size
-        var color = row % 2 == 0 ? col % 2 == 0 ? color1 : color2 : col % 2 != 0 ?  color1 : color2
-        this.tiles[row][col] = new Tile(x, y, row, col, color, tile_size)
+    // Add player one pieces to board
+    var row = 0, col = 0;
+    for(var p = 1; p <= 12; p++) {
+      var piece = this._players[0].pieces[p - 1];
+      var tile = this._board[row][col];
+      piece.setPosition(tile);
+      tile.piece = piece;
+      col += 2;
+      if(p % 4 == 0) {
+        row++;
+        col = row % 2 == 0 ? 0 : 1;
       }
     }
+
+
+    // Add player two pieces to board
+    row = 5, col = 1;
+    for(var p = 1; p <= 12; p++) {
+      var piece = this._players[1].pieces[p - 1];
+      var tile = this._board[row][col];
+      piece.setPosition(tile);
+      tile.piece = piece;
+      col += 2;
+      if(p % 4 == 0) {
+        row++;
+        col = row % 2 == 0 ? 0 : 1;
+      }
+    }
+
+    // Add event handlers
+    // Click handler
+    this._canvas.addEventListener("click", e => this.handleClick(e));
+    // Hover handler
+    this._canvas.addEventListener("mousemove", e => this.handleHover(e));
   }
 
-  setup() {
-    this.removePieces()
-    for(var player = 0; player < 2; player++) {
-      var row = 0
-      var row_step = 1
-      var end_row = 3
-      var color = "red"
+  handleClick(e) {
+    var rect = this._canvas.getBoundingClientRect();
+    var x = (e.clientX - rect.left);
+    var y = (e.clientY - rect.top);
 
-      if(player == 1) {
-        row = 7
-        row_step = -1
-        end_row = 4
-        color = "yellow"
-      }
-
-      while(row != end_row) {
-        for(var n = 0; n < this.columns / 2; n++) {
-          var col = n * 2
-          if(row % 2 != 0) {
-            col++
+    // Find clicked tile
+    for(var row = 0; row < 8; row++) {
+      for(var col = 0; col < 8; col++) {
+        // Is this click the moving piece?
+        if(this._movePiece == null) {
+          // Find the tile this click intersects and if it's the current players piece
+          if(this._board[row][col].intersect(x,y) && this._board[row][col].piece !== null && this._board[row][col].piece.player === this._currentPlayer) {
+            var tile = this._board[row][col];
+            var piece = tile.piece;
+            this._movePiece = piece;
+            piece.selected = true;
+            this._availableMoves = this.findLegalJumps(piece.legalMoves());
           }
-          var piece = new Piece(col * this.tile_size + this.tile_size / 2, row * this.tile_size + this.tile_size / 2, color, player)
-          this.tiles[row][col].piece = piece
+        } else {
+          // Where should we move the move piece?
+          if(this._board[row][col].intersect(x,y)) {
+            var tile = this._board[row][col];
+            var piece = tile.piece;
+            // Did we click on the move piece again, if so, unselect it.
+            if(this._movePiece === piece) {
+              this._movePiece = null;
+              piece.selected = false;
+              this._availableMoves = false;
+            } else if(contains(this._availableMoves, tile)) {
+              this._moveSpot = tile;
+            }
+          }
         }
-        row = row + row_step
       }
     }
   }
 
-  removePieces() {
-    for(var row = 0; row < this.rows; row++) {
-      for(var col = 0; col < this.columns; col++) {
-        this.tiles[row][col].piece = undefined
-      }
+  // returns an array of tiles that the selected piece can jump
+  // first given a list of legal moves given by the piece
+  findLegalJumps(moves) {
+    var tiles = [];
+    var row = 0;
+    var col = 0;
+    for(var i = 0; i < moves.length; i++) {
+      row = moves[i][0];
+      col = moves[i][1];
+      var tile = this._board[row][col];
+      tiles.push(tile);
     }
+
+    return tiles;
+  }
+
+  handleHover(e) {
+    var info = document.getElementById("hover-info");
+    var rect = this._canvas.getBoundingClientRect();
+    var x = (e.clientX - rect.left);
+    var y = (e.clientY - rect.top);
+    var msg = x + ", " + y;
+    info.innerHTML = msg;
+  }
+
+  nextPlayer() {
+    this._movePiece = null;
+    this._moveSpot = null;
+    this._availableMoves = [];
+    this._currentPlayer = this._currentPlayer == this._players[1] ? this._players[0] : this._players[1];
+  }
+
+  start() {
+    window.requestAnimationFrame(() => this.update());
+  }
+
+  update() {
+    this.draw();
+    if(this._movePiece != null && this._moveSpot != null) {
+      var row = this._movePiece._row;
+      var col = this._movePiece._col;
+      this._board[row][col].piece = null;
+      this._moveSpot.piece = this._movePiece;
+      this._movePiece.setPosition(this._moveSpot)
+      this.nextPlayer();
+    }
+    window.requestAnimationFrame(() => this.update());
   }
 
   draw() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-    for ( var row = 0; row < this.rows; row++ ) {
-      for ( var col = 0; col < this.columns; col++ ) {
-        var tile = this.tiles[row][col]
-        this.ctx.beginPath()
-        this.ctx.rect(tile.x, tile.y, tile.size, tile.size)
-        this.ctx.fillStyle = tile.color
-        this.ctx.fill()
-        if(tile.clicked) {
-          this.ctx.lineWidth = 2
-          this.ctx.strokeStyle = "blue"
-          this.ctx.stroke()
-        } else if(tile.highlight) {
-          this.ctx.lineWidth = 2
-          this.ctx.strokeStyle = "orange"
-          this.ctx.stroke()
-        }
+    this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+    // draw board
+    for(var row = 0; row < 8; row++) {
+      for(var col = 0; col < 8; col++) {
+        var highlight = contains(this._availableMoves, this._board[row][col]);
+        this._board[row][col].draw(this._ctx, highlight);
       }
     }
-    for ( var row = 0; row < this.rows; row++ ) {
-      for ( var col = 0; col < this.columns; col++ ) {
-        var tile = this.tiles[row][col]
-        if (tile.piece !== undefined) {
-          var piece = tile.piece
-          this.ctx.beginPath()
-          this.ctx.arc(piece.x, piece.y, tile.size / 2.5, 2 * Math.PI, false)
-          this.ctx.fillStyle = piece.color
-          this.ctx.fill()
-          /*
-          this.ctx.lineWidth = 3
-          this.ctx.strokeStyle = "black"
-          this.ctx.stroke()
-          */
-        }
+    // draw pieces, only if still in game
+    for(var p = 0; p < this._pieces.length; p++) {
+      var piece = this._pieces[p]
+      if(piece.inPlay) {
+        piece.draw(this._ctx);
       }
     }
   }
 }
 
 class Piece {
-  constructor(x, y, color, player) {
-    this.x = x
-    this.y = y
-    this.color = color
-    this.player = player
-    this.clicked = false
+  constructor(player, direction) {
+    this._row = -1;
+    this._col = -1;
+    this._inPlay = true;
+    this._player = player;
+    this._isKing = false;
+    this._tile = null
+    this._move = 1;
+    this._rowDirection = direction;
+    this._selected = false;
+  }
+
+  set selected(select) {
+    this._selected = select;
+  }
+
+  get player() {
+    return this._player;
+  }
+
+  setPosition(tile) {
+    this._row = tile._row;
+    this._col = tile._col;
+    this._tile = tile
+  }
+
+  get inPlay() {
+    return this._inPlay;
+  }
+
+  jumped() {
+    this._inPlay = false;
+    this._row = -1;
+    this._col = -1;
+    this._tile.piece = null;
+  }
+
+  // A guess at legal moves allowed, does not take into account other pieces
+  legalMoves() {
+    var moves = [];
+    var moveRow = this._row + this._rowDirection;
+    if((this._rowDirection == 1 && moveRow <= 7) || (this._rowDirection == -1 && moveRow >= 0)) {
+      if(this._col + 1 <= 7) {
+        moves.push([moveRow, this._col + 1]);
+      }
+      if(this._col - 1 >= 0) {
+        moves.push([moveRow, this._col - 1]);
+      }
+    }
+
+    if(this._king) {
+      moveRow = this._row + (-1 * this._rowDirection);
+      if(this._col + 1 <= 7) {
+        moves.push([moveRow, this._col + 1]);
+      }
+      if(this._col - 1 >= 0) {
+        moves.push([moveRow, this._col - 1]);
+      }
+    }
+    return moves;
+  }
+
+  draw(ctx) {
+    var x = this._col * this._tile.width + this._tile.width / 2;
+    var y = this._row * this._tile.height + this._tile.width / 2;
+    ctx.beginPath();
+    ctx.arc(x, y, 25, 2 * Math.PI, false);
+    ctx.fillStyle = this._player.pieceColor;
+    ctx.fill();
   }
 }
 
 class Tile {
-  constructor(x, y, row, col, color, size) {
-    this.x = x
-    this.y = y
-    this.row = row
-    this.col = col
-    this.color = color
-    this.size = size
-
-    this.piece = undefined
-    this.highlight = false
-    this.clicked = false
+  constructor(row, col, color) {
+    this._row = row;
+    this._col = col;
+    this._color = color;
+    this._piece = null;
+    this._width = 75;
+    this._height = 75;
   }
 
-  entered(x, y) {
-    return this.x <= x && this.x + this.size >= x && this.y <= y && this.y + this.size >= y
+  get piece() {
+    return this._piece;
   }
+
+  set piece(piece) {
+    this._piece = piece;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  draw(ctx, highlight) {
+    var x = this._col * this._width;
+    var y = this._row * this._height;
+    ctx.beginPath();
+    ctx.rect(x, y, this._width, this._height);
+    ctx.fillStyle = this._color;
+    ctx.fill();
+    if(!highlight) {
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "black";
+      ctx.stroke();
+    } else {
+      ctx.lineWidth = 4;
+      ctx.strokeStyle = "blue";
+      ctx.stroke();
+    }
+  }
+
+  intersect(x, y) {
+    return (this._row * this._height) < y
+        && (this._row * this._height + this._height) > y
+        && (this._col * this._width) < x
+        && (this._col * this._width + this._width) > x
+  }
+}
+
+// Find if object in Array
+function contains(array, obj) {
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] === obj) {
+      return true;
+    }
+  }
+  return false;
 }
